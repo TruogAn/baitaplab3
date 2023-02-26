39f7bed7c4e30b4056ab885ad2edcb753875b360
TruogAn <125165785+TruogAn@users.noreply.github.com>
2023-02-26 20:28:05 +0700

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,38 @@
+package Lab03;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private float width;
+		private float height;
+
+		public Triangle() {
+			this.width = 0.0f;
+			this.height = 0.0f;
+		}
+
+		public Triangle(float width, float height) {
+			if (height > 0 && width > 0) {
+				this.height = height;
+				this.width = width;
+			}
+		}
+		public float getWidth() {
+			return this.width;
+		}
+		public void setWidth(float width) {
+			this.width = width;
+		}
+		public float getHeight() {
+			return this.height;
+		}
+		public void setHeight(float height) {
+			this.height = height;
+		}
+		public String toString() {
+			return ("Triangle (Width = " + this.width + ", Height = " + this.height + ")");
+			
+	}
+
+}
